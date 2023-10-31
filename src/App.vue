fca1406929c8d495efa8ce2b2ae6a4a568ff22ee
chriswoodcn <chriswoodcn@aliyun.com>
2023-11-01 06:42:21 +0800

    [update] cesium初體驗

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,18 @@ import { ref, onMounted } from "vue";
 // import { renderer, animate } from "./components/startup";
 // import { renderer, animate } from "./components/envmap";
 // import { renderer, animate } from "./components/points";
-import { renderer, animate } from "./components/raycaster";
-animate();
-const mapRef = ref();
+// import { renderer, animate } from "./components/raycaster";
+// animate();
+// const mapRef = ref();
+// onMounted(() => {
+//   mapRef.value.appendChild(renderer.domElement);
+// });
+import * as Cesium from "cesium";
 onMounted(() => {
-  mapRef.value.appendChild(renderer.domElement);
+  const viewer = new Cesium.Viewer("cesium");
 });
 </script>
 
 <template>
-  <div ref="mapRef" class="w-full h-full"></div>
+  <div id="cesium" ref="mapRef" class="w-full h-full"></div>
 </template>
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import {defineConfig} from 'vite'
-import vue from '@vitejs/plugin-vue'
-import UnoCSS from 'unocss/vite'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import UnoCSS from "unocss/vite";
+import cesium from "vite-plugin-cesium";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [UnoCSS(), vue()],
-})
+  plugins: [UnoCSS(), vue(), cesium()],
+});
